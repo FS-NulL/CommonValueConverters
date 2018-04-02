63d011688e9fd34c948fb4e57bd7849c7093af63
Jonathan <jonathan.norton@gmail.com>
2018-04-02 17:33:56 +0100

    Preventing more exceptions.

--- ColorToBrushConverter.cs
+++ ColorToBrushConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
 using System.Windows.Media;
 
@@ -9,12 +10,16 @@ namespace CommonValueConverters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            if (!(value is Color)) return DependencyProperty.UnsetValue;
+
             var c = (Color)value;
             return new SolidColorBrush(c);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            if (!(value is SolidColorBrush)) return DependencyProperty.UnsetValue;
+
             var b = (SolidColorBrush)value;
             return b.Color;
         }
