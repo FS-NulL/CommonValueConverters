b8c369719769fbc9cb9a75f71d62e9be7a858636
Jonathan <jonathan.norton@gmail.com>
2018-04-02 19:36:37 +0100

    Corrections to StringEqualityConverter

--- StringEqualityConverter.cs
+++ StringEqualityConverter.cs
@@ -1,13 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Text;
 using System.Windows.Data;
 using System.Windows;
 
 namespace CommonValueConverters
 {
-    class StringEqualityConverter : IValueConverter
+    public class StringEqualityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
@@ -17,7 +15,7 @@ namespace CommonValueConverters
 
             string s = (string)value;
 
-            if (string.IsNullOrEmpty(s)) return false;
+            if (s == null) return false;
 
             if (s == (string)parameter) return true;
 
@@ -26,14 +24,7 @@ namespace CommonValueConverters
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (parameter == null) return DependencyProperty.UnsetValue;
-            if (!(parameter is string)) return DependencyProperty.UnsetValue;
-            if (!(value is bool)) return DependencyProperty.UnsetValue;
-
-            bool b = (bool)value;
-
-            if (b) return (string)parameter;
-            else return string.Empty;
+            throw new NotImplementedException();
         }
     }
 }
