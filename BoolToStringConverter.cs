584f1c13de6e45580c14945732af51908665486d
Jonathan <jonathan.norton@gmail.com>
2018-06-14 18:33:19 +0100

    Added BoolToStringConverter

---
+++ BoolToStringConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace CommonValueConverters
+{
+    class BoolToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is bool)) return DependencyProperty.UnsetValue;
+
+            bool b = (bool)value;
+
+            if (b) return TrueString;
+            else return FalseString;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is string)) return DependencyProperty.UnsetValue;
+
+            string c = (string)value;
+
+            if (c == TrueString) return true;
+            else return false;
+        }
+
+        public string TrueString { get; set; } = "True";
+        public string FalseString { get; set; } = "False";
+    }
+}
