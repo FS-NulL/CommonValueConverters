fcced80ddf610fa3cb358ce098d2f9aa569bc145
Jonathan <jonathan.norton@gmail.com>
2018-10-27 18:53:54 +0100

    Added BoolToIntConverter

---
+++ BoolToIntConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace CommonValueConverters
+{
+    public class BoolToIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is bool)) return DependencyProperty.UnsetValue;
+
+            bool b = (bool)value;
+
+            if (b) return TrueValue;
+            else return FalseValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is int)) return DependencyProperty.UnsetValue;
+
+            int c = (int)value;
+
+            if (c == TrueValue) return true;
+            else return false;
+        }
+
+        public int TrueValue  { get; set; } = 1;
+        public int FalseValue { get; set; } = 0;
+    }
+}
